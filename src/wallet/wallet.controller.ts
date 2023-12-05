05fa4d24b452f34b6fb83583d906d4ad82442175
RafaelFatkulin <armistman@vk.com>
2023-12-06 01:08:04 +0500

    [add] update|delete wallets

--- src/wallet/wallet.controller.ts
+++ src/wallet/wallet.controller.ts
@@ -9,13 +9,15 @@ import {
   HttpCode,
   HttpStatus,
   UseGuards,
-  UsePipes,
-  ValidationPipe,
+  Put,
+  ParseIntPipe,
 } from '@nestjs/common';
 import { WalletService } from './wallet.service';
 import { ApiBearerAuth } from '@nestjs/swagger';
 import { AuthGuard } from '@nestjs/passport';
-import { CreateWalletRequest } from './models';
+import { CreateWalletRequest, UpdateWalletRequest } from './models';
+import { User } from 'src/user/user.decorator';
+import { AuthUser } from 'src/auth/auth-user';
 
 @Controller('wallet')
 export class WalletController {
@@ -27,5 +29,28 @@ export class WalletController {
   @UseGuards(AuthGuard())
   async create(@Body() createRequest: CreateWalletRequest): Promise<void> {
     await this.walletService.createWallet(createRequest);
+  }
+
+  @ApiBearerAuth()
+  @Put(':id')
+  @HttpCode(HttpStatus.OK)
+  @UseGuards(AuthGuard())
+  async update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateRequest: UpdateWalletRequest,
+    @User() user: AuthUser,
+  ): Promise<void> {
+    await this.walletService.updateWallet(id, user.id, updateRequest);
+  }
+
+  @ApiBearerAuth()
+  @Delete(':id')
+  @HttpCode(HttpStatus.OK)
+  @UseGuards(AuthGuard())
+  async delete(
+    @Param('id', ParseIntPipe) id: number,
+    @User() user: AuthUser,
+  ): Promise<void> {
+    await this.walletService.deleteWallet(id, user.id);
   }
 }
--- src/wallet/wallet.service.ts
+++ src/wallet/wallet.service.ts
@@ -1,27 +1,106 @@
-import { Injectable, ConflictException, Logger } from '@nestjs/common';
-import { CreateWalletRequest, WalletResponse } from './models';
+import {
+  Injectable,
+  ConflictException,
+  Logger,
+  NotFoundException,
+  ForbiddenException,
+} from '@nestjs/common';
+import {
+  CreateWalletRequest,
+  UpdateWalletRequest,
+  WalletResponse,
+} from './models';
 import { PrismaService } from 'src/common/services/prisma.service';
 
 @Injectable()
 export class WalletService {
   constructor(private readonly prisma: PrismaService) {}
 
+  async getWallet(walletId: number): Promise<WalletResponse> {
+    try {
+      return await this.prisma.wallet.findUnique({ where: { id: walletId } });
+    } catch (err) {
+      Logger.error(JSON.stringify(err));
+      throw new NotFoundException();
+    }
+  }
+
   async createWallet(
     createWalletRequest: CreateWalletRequest,
   ): Promise<WalletResponse> {
     try {
-      const { name, userId, balance } = createWalletRequest;
       const createdWallet = await this.prisma.wallet.create({
-        data: {
-          name,
-          userId,
-          balance,
-        },
+        data: createWalletRequest,
       });
 
       return WalletResponse.fromWalletEntity(createdWallet);
     } catch (err) {
       Logger.error(JSON.stringify(err));
+      throw new ConflictException();
+    }
+  }
+
+  async updateWallet(
+    walletId: number,
+    userId: number,
+    updateWalletRequest: UpdateWalletRequest,
+  ): Promise<WalletResponse> {
+    try {
+      const walletToUpdate = await this.getWallet(walletId);
+
+      if (!walletToUpdate) {
+        throw new NotFoundException();
+      }
+
+      if (userId !== walletToUpdate.userId) {
+        throw new ForbiddenException();
+      }
+
+      const updatedWallet = await this.prisma.wallet.update({
+        where: { id: walletId },
+        data: updateWalletRequest,
+      });
+
+      return WalletResponse.fromWalletEntity(updatedWallet);
+    } catch (err) {
+      Logger.error(JSON.stringify(err));
+
+      if (
+        err instanceof NotFoundException ||
+        err instanceof ForbiddenException
+      ) {
+        throw err;
+      }
+
+      throw new ConflictException();
+    }
+  }
+
+  async deleteWallet(walletId: number, userId: number): Promise<void> {
+    try {
+      const walletToDelete = await this.getWallet(walletId);
+
+      if (!walletToDelete) {
+        throw new NotFoundException();
+      }
+
+      if (userId !== walletToDelete.userId) {
+        throw new ForbiddenException();
+      }
+
+      await this.prisma.wallet.delete({
+        where: { id: walletId },
+      });
+    } catch (err) {
+      Logger.error(JSON.stringify(err));
+
+      if (
+        err instanceof NotFoundException ||
+        err instanceof ForbiddenException
+      ) {
+        throw err;
+      }
+
       throw new ConflictException();
     }
   }
